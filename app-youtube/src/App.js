9b24d25d750fa275b05f15447a8e2f99e0bbab96
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-02-13 19:52:31 -0300

    ...

--- app-youtube/src/App.js
+++ app-youtube/src/App.js
@@ -1,26 +1,35 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-<div className='main'>
-
-<div className='menu'>
-  <div className='logo'>
-    <h3>Sem nome</h3>
-  </div>
-<div className='item-menu'>
-  <a href='#'>login</a>
-</div>
-
-</div>
-
-
-
-</div>
+      <div className="main">
+        <div className="center">
+          <div className="menu">
+            <div className="logo">
+              <h3>Sem nome</h3>
+            </div>
+            <div className="item-menu">
+              <a href="#">login</a>
+            </div>
+          </div>
+        </div>
+        <div className="form">
+          <form>
+            <h2>Entre em contato!</h2>
+            <div className="items-form">
+              <input type="text" />
+              <input type="text" />
+              <input type="text" />
+              <input type="text" />
+              <input type="submit" />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
