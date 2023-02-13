21f4fcdf2fd672b9be126fdd8010c0e0dac2340b
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-02-13 19:00:10 -0300

    html

--- app-youtube/src/App.js
+++ app-youtube/src/App.js
@@ -4,7 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+<div className='main'>
 
+<div className='menu'>
+  <div className='logo'>
+    <h3>Sem nome</h3>
+  </div>
+<div className='item-menu'>
+  <a href='#'>login</a>
+</div>
+
+</div>
+
+
+
+</div>
     </div>
   );
 }
