3e80ff958ea5440bfe4ccad7c732aef9a8431582
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-02-15 17:18:45 -0300

    html feito

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,9 +1,26 @@
-
-
+import { FiSearch } from "react-icons/fi";
+import './style.css';
 function App() {
   return (
-    <div className="App">
-      <h1>Teste Projeto</h1>
+    <div className="container">
+      <h1>Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu cep..." />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 8955488</h2>
+
+        <span>Rua Teste</span>
+        <span>Complemento: asdasdasdadsa</span>
+        <span>asdasdq131231</span>
+        <span>belo horizonte - mg</span>
+      </main>
     </div>
   );
 }
