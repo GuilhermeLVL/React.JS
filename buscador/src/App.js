839047dd8d4d7d0c1f934fef914237bfaf5a9acc
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-02-15 17:39:37 -0300

    ..

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -3,7 +3,7 @@ import './style.css';
 function App() {
   return (
     <div className="container">
-      <h1>Buscador CEP</h1>
+      <h1 className="title">Buscador CEP</h1>
 
       <div className="containerInput">
         <input type="text" placeholder="Digite seu cep..." />
