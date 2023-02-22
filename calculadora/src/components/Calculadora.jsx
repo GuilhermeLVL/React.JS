5600141758afea879e6cfb40f62a9c9236185fc7
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-02-22 17:58:19 -0300

    cores e alinhamento

--- calculadora/src/components/Calculadora.jsx
+++ calculadora/src/components/Calculadora.jsx
@@ -9,28 +9,30 @@ export default function Calculadora() {
       <Box m={5} />
       <Container maxWidth="xs">
         <div className="wrapper">
+          <h1 className="resultado">01</h1>
           <button>AC</button>
           <button>+/-</button>
           <button>%</button>
-          <button>/</button>
+          <button className="orange">/</button>
 
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>X</button>
+          <button className="gray">7</button>
+          <button className="gray">8</button>
+          <button className="gray">9</button>
+          <button className="orange">X</button>
 
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
+          <button className="gray">4</button>
+          <button className="gray">5</button>
+          <button className="gray">6</button>
+          <button className="orange">-</button>
 
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
+          <button className="gray">1</button>
+          <button className="gray">2</button>
+          <button className="gray">3</button>
+          <button className="orange">+</button>
 
-          <button>0</button>
-          <button>,</button>
+          <button className="gray">0</button>
+          <button className="gray">,</button>
+          <button className="gray" style={{visibility:"hidden"}}>,</button>
           <button>=</button>
         </div>
       </Container>
