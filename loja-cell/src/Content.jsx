cc9567cbbfc47c7333f8389c2bd134ccdea318f1
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-03-01 16:21:04 -0300

    ,

--- loja-cell/src/Content.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {Switch, Route} from 'react-router-dom'
-export const Content = () =>{
-    return (
-        <Switch>
-            <Route path='/store' component={S}
-        </Switch>
-    )
-}
