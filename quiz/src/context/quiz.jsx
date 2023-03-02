7b5972032726a61af00198cf008e8b9e5ff429bd
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-03-02 19:05:27 -0300

    Update quiz.jsx

--- quiz/src/context/quiz.jsx
+++ quiz/src/context/quiz.jsx
@@ -1,4 +1,22 @@
-import { createContext } from "preact";
+import { createContext, useReducer } from "preact";
+
+import questions from '../data/questions';
+const STAGES = ["Start","Playing","End"]
+
+const initialState = {
+    gameStage:STAGES[0],
+    question
+}
+const quizReducer = (state, action) => {
+    switch(action.type) {
+        console.log(state, action)
+        case "CHANGE_STATE":
+            return state;
+
+            default:
+                return state;
+    }
+}
 export const QuizContext = createContext()
 
 export const QuizProider = ({children}) => {
