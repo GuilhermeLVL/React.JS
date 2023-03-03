ba4a02b2b5834614d78702285d6cc1011fc53ea1
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-03-03 17:29:50 -0300

    .

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,9 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Primeiro from './components/Primeiro';
+import ComParametro from './components/ComParametro';
 
 ReactDOM.render(
 
-    <Primeiro></Primeiro>,
+    <>
+    <Primeiro/>
+    <ComParametro titulo="Esse e o titulo"
+    subtitulo="Esse e o subtitulo"/>
+    </>
+    ,
     document.getElementById('root')
 )
