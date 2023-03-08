2b229c225f801afa8f6aeb5ecb74077a0d49a997
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-03-08 18:19:28 -0300

    Update Contador.jsx

--- fundamentos-react/src/components/contador/Contador.jsx
+++ fundamentos-react/src/components/contador/Contador.jsx
@@ -11,16 +11,30 @@ constructor(props){
     }
 }
 
+inc = () => {
+this.setState({
+    valor: this.state.valor + this.state.passo 
+})
+}
+
+dec = () => {
+    this.setState({
+        valor: this.state.valor - this.state.passo
+    })
+}
+
     render(){
         return(
             <div>
                 <h2>Contador</h2>
                 <h4>Passo:{this.state.passo}</h4>
                 <h4>Valor:{this.state.valor}</h4>
+                <input type="number" value={this.state.passo} 
+                onChange={e => this.setState({passo: +e.target.value})}/>
 
                 <div>
-                    <button>+</button>
-                    <button>-</button>
+                    <button onClick={this.inc}>+</button>
+                    <button onClick={this.dec}>-</button>
                 </div>
             </div>
 
