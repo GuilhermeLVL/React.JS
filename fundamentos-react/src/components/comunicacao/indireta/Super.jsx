b1633a255981cc1a7a3baa2208189d1922a02709
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-03-08 17:04:36 -0300

    Update Super.jsx

--- fundamentos-react/src/components/comunicacao/indireta/Super.jsx
+++ fundamentos-react/src/components/comunicacao/indireta/Super.jsx
@@ -3,14 +3,15 @@ import Sub from './Sub'
 
 export default props =>
 {
+    const [texto, setTexto] = useState(0)
+    const [num, setNum] = useState(0)
 
     function quandoClicar(valorGerado){
-        console.log('acao!!!!!!')
-        console.log(valorGerado)
+      setNum(valorGerado)
     }
     return(
         <div>
-<h4>Valor</h4>
+<h4>Valor:{num}</h4>
 <Sub quandoClicar={quandoClicar}></Sub>
 </div>
     )
