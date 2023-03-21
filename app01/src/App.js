061eb9d8307d9d8a887d16189fb90582394c3a27
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-03-21 17:01:44 -0300

    a

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,15 +1,18 @@
 import React,{useState} from "react";
+import Numero from "./componentes/Numero";
 import './App.css'
 
   export default function App(){
 
 const[num, setNum] = useState(10)
 
-let n1 = 10
+let n1 = 12
+
 
   return(
 <>
-  <p>Valor do state num:{n1}</p>
+  <p>Valor do state num em app:{num}</p>
+  <Numero num={num}></Numero>
 </>
   )
 }
---
+++ app01/src/componentes/Numero.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Numero(props) {
+  return (
+    <p>
+      <p>Valor do state num em numero:{num}</p>
+      <button onClick={}>100</button>
+    </p>
+  );
+}
