aa7d7972cd6ffaf16c794aca9a438e12666248e5
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-03-21 14:12:51 -0300

    s

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css'
+import Relogio from "./componentes/Relogio";
 
   export default function App(){
 const textoDestaque ={
@@ -12,6 +13,7 @@ const textoDestaque ={
   return(
 <>
     <section className="caixa">
+      <Relogio ></Relogio>
     <h1 style={{color:'red',fontSize:'2em'}}>Ola mundo</h1>
     <h2 style={textoDestaque}>Curso de react</h2>
     <p className="texto">asdasdasdasdasdasdasdasd</p>
