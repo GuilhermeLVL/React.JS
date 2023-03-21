80e523a187089c26a74e61abc31cf598c7c494e3
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-03-21 16:50:23 -0300

    a

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,24 +1,15 @@
-import React from "react";
+import React,{useState} from "react";
 import './App.css'
-import Relogio from "./componentes/Relogio";
 
   export default function App(){
-const textoDestaque ={
-   color:'#00f',
-   fontSize:'1.9em',
-   backgroundColor:'purple',
-   borderRadius: '50px'
-}
+
+const[num, setNum] = useState(10)
+
+let n1 = 10
 
   return(
 <>
-    <section className="caixa">
-      <Relogio ></Relogio>
-    <h1 style={{color:'red',fontSize:'2em'}}>Ola mundo</h1>
-    <h2 style={textoDestaque}>Curso de react</h2>
-    <p className="texto">asdasdasdasdasdasdasdasd</p>
-    <a href="">Paradoxo</a>
-    </section>
+  <p>Valor do state num:{n1}</p>
 </>
   )
 }
