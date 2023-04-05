8d3f91d5590da257e7cf4781a4d3eb93a8d6d6d7
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-05 15:58:02 -0300

    Update App.js

--- app01/src/App.js
+++ app01/src/App.js
@@ -2,16 +2,19 @@ import React,{useState} from 'react'
 
   export default function App(){
 
-    const carros = ['A1233','B3213','C4324','D23123','E42342','F31231','G234234','H123','I13213']
+   
+
+    
 
     const listaCarros = carros.map(
-      
-      (c) => <p>{c}</p>
+
+      (c,i) => <li key={i}>{i}-{c}</li>
     )
   return(
 <>
 
-{listaCarros}
+
+<ul> {listaCarros}</ul>
 </>
 
 );
