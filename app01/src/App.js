d940d043beeb289aa62ed2852278dabad7448425
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-05 14:09:32 -0300

    Update App.js

--- app01/src/App.js
+++ app01/src/App.js
@@ -2,40 +2,16 @@ import React,{useState} from 'react'
 
   export default function App(){
 
+    const carros = ['A1233','B3213','C4324','D23123','E42342','F31231','G234234','H123','I13213']
 
-  const [cor,setCor]=useState(1)
-
-  const vermelho ={color:'#f00'}
-  const verde ={color:'#0f0'}
-  const azul ={color:'#00f'}
-
-
-  const retornaCor=(c)=>{
-    if(c==1){
-      return vermelho
-    }else if(c==2){
-      return verde
-    }else{
-      return azul
-    }
-  }
-
-
-  const mudaCor=()=>{
-    setCor(cor+1)
-    if(cor > 2){
-      setCor(1)
-    }
-  }
-
-  setInterval(mudaCor,1000)
+    const listaCarros = carros.map(
+      
+      (c) => <p>{c}</p>
+    )
   return(
 <>
 
-<h1 style={retornaCor(cor)}>Olaaaaaaaa</h1>
-
-<button onClick={()=>mudaCor()}>Muda cor</button>
-
+{listaCarros}
 </>
 
 );
