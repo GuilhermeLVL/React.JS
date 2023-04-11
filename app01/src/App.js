fdfc880cdac334647cb3d0baa8f8792b25f3485f
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-11 16:50:05 -0300

    Update App.js

--- app01/src/App.js
+++ app01/src/App.js
@@ -2,12 +2,22 @@ import React,{useState} from 'react'
 
   export default function App(){
 
-    const [nome,setNome]= useState('')
-   
+    const [nome,setNome]= useState(' ')
+
+
+
+    const hanleChangeNome = (e) => {
+      setNome(e.target.value) 
+    }
+
+
   return(
 <>
 <label>Digite seu nome</label>
-<input type='text' name='fnome' value={nome} onChange={(e) => setNome(e.target.value)}></input>
+<input type='text'
+ name='fnome'
+  value={nome} 
+  onChange={(e) => hanleChangeNome(e)}></input>
 <p>Nome digitado:{nome} </p>
 </>
 
