2dfab614f6fcea807816a7a1d90a068aa3b50237
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-11 16:35:53 -0300

    Update App.js

--- app01/src/App.js
+++ app01/src/App.js
@@ -3,18 +3,11 @@ import React,{useState} from 'react'
   export default function App(){
 
    
-
-    
-
-    const listaCarros = carros.map(
-
-      (c,i) => <li key={i}>{i}-{c}</li>
-    )
   return(
 <>
-
-
-<ul> {listaCarros}</ul>
+<label>Digite seu nome</label>
+<input type='text' name='fnome'></input>
+<p>Nome digitado: </p>
 </>
 
 );
