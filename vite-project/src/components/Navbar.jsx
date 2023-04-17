58f11705f20e3053820c4db9f031d7df2e3fd03a
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-17 17:51:06 -0300

    Update Navbar.jsx

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -48,6 +48,13 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          
+          <img src={menu} alt="menu" />
+        </div>
+
+
       </div>
     </nav>
   );
