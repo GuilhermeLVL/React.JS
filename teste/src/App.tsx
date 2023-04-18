e7843660e72c965bb597fe308c117e9850705bdd
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-18 17:39:44 -0300

    Update App.tsx

--- teste/src/App.tsx
+++ teste/src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState }from 'react'
 import './App.css'
+
+interface ClikedProps{
+  clientX: Number
+  clientY: Number
+}
 
 function App() {
 
+  const [clickPoints, setClickPoints] = useState<ClikedProps[]>([])
+
  function getCordenadas(e: React.MouseEvent<HTMLElement>){
-  console.log(e)
+  const {clientX, clientY} = e
+
+  setClickPoints([...clickPoints,{clientX, clientY}])
  }
   return <div className="App" onClick={getCordenadas}></div>
 
