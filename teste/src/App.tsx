110ff7d3a45922fa42cc87056d969fe21a1177ec
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-18 17:15:20 -0300

    a

--- teste/src/App.tsx
+++ teste/src/App.tsx
@@ -1,9 +1,10 @@
-
+import React from 'react'
 import './App.css'
 
 function App() {
- function getCordenadas(e: React.MouseEvent<HTMLElement>){
-console.log(e)
+
+ function getCordenadas(e:React.MouseEvent<HTMLElement>){
+  console.log(e)
  }
   return <div className="App" onClick={getCordenadas}></div>
 
