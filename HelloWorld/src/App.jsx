2b34d7a717392053aff2c270d72647f7102555a5
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-26 17:16:49 -0300

    Update App.jsx

--- HelloWorld/src/App.jsx
+++ HelloWorld/src/App.jsx
@@ -6,6 +6,18 @@ function App() {
   return (
     <>
     <h1>Hello World!</h1>
+
+    <table>
+      <tr>
+        <th>Name</th>
+      </tr>
+      <tr>
+        <td>John</td>
+      </tr>
+      <tr>
+        <td>Elsa</td>
+      </tr>
+    </table>
     </>
   )
 }
