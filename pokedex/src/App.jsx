ef75429368c4108aaedd1687fb793d68afdb6f7d
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-27 16:20:22 -0300

    .

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 
 import './App.css'
 import { Home } from './pages/Home'
+import NavBar from './components/NavBar'
+import PokemonCard from './components/PokemonCard'
 
 function App() {
 
   return (
     <>
+      
+      <NavBar></NavBar>
+      <PokemonCard></PokemonCard>
       <Home></Home>
     </>
   )
