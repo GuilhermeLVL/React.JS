0ad7aee147ad40c57f7fe0fb573d858c8011556e
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-27 16:50:58 -0300

    Update index.jsx

--- pokedex/src/components/PokemonCard/index.jsx
+++ pokedex/src/components/PokemonCard/index.jsx
@@ -19,17 +19,21 @@ export default function PokemonCard() {
           <Typography gutterBottom variant="h5" component="div">
             Lizard
           </Typography>
+          {/* 
           <Typography variant="body2" color="text.secondary">
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
           </Typography>
+          */}
         </CardContent>
       </CardActionArea>
+      {/* 
       <CardActions>
         <Button size="small" color="primary">
           Share
         </Button>
       </CardActions>
+      */}
     </Card>
   );
 }
