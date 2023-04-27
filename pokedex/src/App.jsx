17e1dc83bc1ba0f116edaf5d689a8d242f06f790
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-04-27 18:04:56 -0300

    Update App.jsx

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,52 +1,52 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.css'
-import { Home } from './pages/Home'
-import NavBar from './components/NavBar'
-import PokemonCard from './components/PokemonCard'
-import { Container, Grid } from '@mui/material'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import { Home } from "./pages/Home";
+import NavBar from "./components/NavBar";
+import PokemonCard from "./components/PokemonCard";
+import { Container, Grid } from "@mui/material";
 
 function App() {
-
-
-const [pokemons, setPokemons] = useState([])
+  const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
-    getPokemons()
-  },[])
+    getPokemons();
+  }, []);
+
+  const getPokemons = () => {
+    var endpoints = []
+
+    for(var i=1; i<50; i++ ){
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+    }
+
+    var response = axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+    .then((res) => setPokemons(res)) 
+    .catch((err) => console.log(err))
+  
+    
+   
 
 
-const getPokemons = () =>{
-
-  axios.get("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0")
-.then((res) => setPokemons(res.data.results))
-.catch((err) => console.log(err))
-
-}
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0")
+      .then((res) => setPokemons(res.data.results))
+      .catch((err) => console.log(err));
+  };
   return (
     <>
-      
       <NavBar></NavBar>
-      <Container maxWidth='false'>
-
+      <Container maxWidth="false">
         <Grid container>
-          {pokemons.map((pokemon, key) =>(
-          
-          <Grid item xs={3} key={key}>
-            <PokemonCard name={pokemon.name}></PokemonCard>
-          </Grid>
-
+          {pokemons.map((pokemon, key) => (
+            <Grid item xs={3} key={key}>
+              <PokemonCard name={pokemon.data.name}></PokemonCard>
+            </Grid>
           ))}
-          
-          
-
         </Grid>
-      
-
       </Container>
-     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
