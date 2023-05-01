a8c4a1ddf9fe104024c43d337b89c4efca0b56a2
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-05-01 18:09:13 -0300

    a

---
+++ ListaDeTarefas/src/components/Catao.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './catao.css'
+export const Catao = () => {
+
+    function criarCard(){
+        
+    }
+
+  return (
+    <>
+        <button onClick={criarCard}>Criar</button>
+        <button>Editar</button>
+        <button>Excluir</button>
+        
+    <div className='container'>
+        <h2>Status</h2>
+
+        
+    </div>
+    
+    </>
+  )
+}
