6561b93f9d2aa111201d2dbacb9d70ebb7c6cafe
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-05-24 18:41:26 -0300

    a

--- Portfolio_HHoradeCodar/src/components/Slidebar.jsx
+++ Portfolio_HHoradeCodar/src/components/Slidebar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import Avatar from '../img/profile-pic (2).png';
+import '../Styles/components/slidebar.sass';
 
 const Slidebar = () => {
   return (
-    <div>Slidebar</div>
+    <aside id="slidebar">
+      <p>imagem</p>
+      <p className="title">Desenvolvedor</p>
+      <p>redes sociais</p>
+      <p>informacao de contato</p>
+      <a href="" className="btn">Download curriculo</a>
+
+
+    </aside>
   )
 }
 
