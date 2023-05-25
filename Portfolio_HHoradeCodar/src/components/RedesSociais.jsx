35f6e42c649ab4ba43cd802cc6d8822ecee663f6
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-05-25 14:22:02 -0300

    a

--- Portfolio_HHoradeCodar/src/components/RedesSociais.jsx
+++ Portfolio_HHoradeCodar/src/components/RedesSociais.jsx
@@ -3,9 +3,21 @@ import '../Styles/components/sociais.sass'
 
 import {FaLinkedinIn, FaGithub, FaInstagram} from 'react-icons/fa';
 
+const socialNetworks = [
+    { name: "linkedin", icon: <FaLinkedinIn /> },
+  { name: "github", icon: <FaGithub /> },
+  { name: "instagram", icon: <FaInstagram /> },
+]
+
 const RedesSociais = () => {
   return (
-    <div>RedesSociais</div>
+   <section id='redes-Sociais'>
+    {socialNetworks.map((network) =>(
+        <a href="#" className='social-btn' id={network.name} key={network.name}>
+            {network.icon}
+        </a>
+    ))}
+   </section>
   )
 }
 
