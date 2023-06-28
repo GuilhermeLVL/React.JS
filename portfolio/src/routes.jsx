c761db351bcd8bc840cd064c184073ff2ae42d89
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-06-28 17:45:10 -0300

    IMPORT

--- portfolio/src/routes.jsx
+++ portfolio/src/routes.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
+import Sobre from "./Pages/Sobre";
+import Projetos from "./Pages/Projetos";
+import Contatos from "./Pages/Contatos";
 
 function AppRoutes(){
     return(
@@ -7,6 +10,9 @@ function AppRoutes(){
         <Routes>
 
             <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/Sobre" element={<Sobre></Sobre>}></Route>
+            <Route path="/Projetos" element={<Projetos></Projetos>}></Route>
+            <Route path="/Contatos" element={<Contatos></Contatos>}></Route>
         </Routes>
         </BrowserRouter>
     )
