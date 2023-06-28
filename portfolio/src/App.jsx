64c03707dd59b811c927d5d724edb21603374f14
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-06-28 16:42:31 -0300

    Criando Home

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,30 +1,10 @@
 import './App.css'
-import Footer from './Components/Footer'
-import Header from './Components/Header'
+
 
 function App() {
 
   return (
-    <>
-     <Header></Header>
-     <section className='container'>
-      <div className='apresentacao'>
-
-        <p>Ola, sou <br/>
-        <span>Guilherme pinheiro</span>
-        <br/> Dev Front-end</p>
-        <button className='btn btn-red'>Saiba mais</button>
-      </div>
-
-
-      <figure>
-        <img className='img-home' src="/developer-red.svg" alt="imagem de home" />
-      </figure>
-
-
-     </section>
-     <Footer></Footer>
-    </>
+  <h1>Home</h1>
   )
 }
 
