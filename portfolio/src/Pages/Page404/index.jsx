a78ed1da6d5e72c88c86a201318b335d4e00c591
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-06-29 18:21:28 -0300

    css 404

--- portfolio/src/Pages/Page404/index.jsx
+++ portfolio/src/Pages/Page404/index.jsx
@@ -1,6 +1,7 @@
 import Container from "../../Components/Container"
 import Footer from "../../Components/Footer"
 import Header from "../../Components/Header"
+import styles from "./Page404.module.css"
 
 function Page404(){
 
@@ -9,10 +10,10 @@ function Page404(){
     <Header></Header>
     <Container>
 
-        <h2>Algo de errado nao esta certo! </h2>
-        <div>
-            <span>404</span><br />
-            <strong> Pagina Nao Localizada</strong>
+        <h2 className={styles.titulo2}>Algo de errado nao esta certo! </h2>
+        <div className={styles.textos}>
+            <span className={styles.texto_grande}>404</span><br />
+            <strong className={styles.texto_vermelho}> Pagina Nao Localizada</strong>
 
         </div>
     </Container>
