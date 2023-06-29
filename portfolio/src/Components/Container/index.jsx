d499afe04a9f5ed2043ae110f6de79e6edbf1e9f
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-06-29 17:06:36 -0300

    Update index.jsx

---
+++ portfolio/src/Components/Container/index.jsx
@@ -0,0 +1,14 @@
+import styles from  './Container.module.css'
+
+function Container(){
+   
+    return(
+        
+    <section className={styles.Container}>
+
+    </section>
+    )
+}
+    
+   
+export default Container
