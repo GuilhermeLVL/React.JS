8822142ee5ea33890cf70a01fbc77632e7fd5f24
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-03 18:45:37 -0300

    1

--- portfolio/src/Pages/Contatos/index.jsx
+++ portfolio/src/Pages/Contatos/index.jsx
@@ -1,6 +1,7 @@
 import Header from "../../Components/Header"
 import Footer from "../../Components/Footer"
 import Container from "../../Components/Container"
+import styles from "./contatos.module.css"
 
 function Contatos(){
 
@@ -9,7 +10,14 @@ function Contatos(){
         <Header></Header>
 
         <Container>
+            <section className={styles.contatos}>
             <h2>Contatos</h2>
+            <h3>Entre em contato</h3>
+            <p>Para que possamos conversar mais sobre</p>
+            <div className={styles.icones}>
+            iconse</div>
+
+            </section>
         </Container>
         
         <Footer></Footer>
