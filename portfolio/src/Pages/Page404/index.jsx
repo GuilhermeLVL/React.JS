08c3cd2485ed62846648673cb1eda007cd7778ff
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-04 14:29:21 -0300

    Update index.jsx

--- portfolio/src/Pages/Page404/index.jsx
+++ portfolio/src/Pages/Page404/index.jsx
@@ -1,14 +1,13 @@
-import Container from "../../Components/Container"
-import Footer from "../../Components/Footer"
-import Header from "../../Components/Header"
+
+
+
 import styles from "./Page404.module.css"
 
 function Page404(){
 
     return(
     <>
-    <Header></Header>
-    <Container>
+    
 
         <h2 className={styles.titulo2}>Algo de errado nao esta certo! </h2>
         <div className={styles.textos}>
@@ -16,8 +15,7 @@ function Page404(){
         <strong className={styles.texto_vermelho}> Pagina Nao Localizada</strong>
 
         </div>
-    </Container>
-    <Footer></Footer>
+    
     </>
     )
 }
