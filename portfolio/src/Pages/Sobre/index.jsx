1eece81717d2f25dcd760b2b8b8a2a9120e0e5a9
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-04 15:37:47 -0300

    Update index.jsx

--- portfolio/src/Pages/Sobre/index.jsx
+++ portfolio/src/Pages/Sobre/index.jsx
@@ -1,12 +1,26 @@
 import styles from './Sobre.module.css'
-
+import avatar from './images/profile-pic (2).png'
 function Sobre(){
 
     return(
         <section className={styles.sobre}>
        
        <div className={styles.bio}>
-        <h2></h2>
+        <img src={avatar} alt="Imagen" className='styles.avatar'/>
+        <div>
+                <h2>Sobre</h2>
+
+                <p>Sou Guilherme pinheiro <br />
+                Dev Front End</p>
+
+                <p>Trabalho com desenvolvimento Web desde 2021.</p>
+
+                <p>Sou apaixonado por transformar ideias em realidade digital.</p>
+
+                <p>Especializado em criação de aplicações dinâmicas e intuitivas, <br />
+                com foco na experiência do usuário.</p>
+
+        </div>
        </div>
 
        <div className={styles.techs}>
