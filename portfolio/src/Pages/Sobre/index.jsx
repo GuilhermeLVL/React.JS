e833c345ec04f323fe353074800f15245cb144f5
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-04 14:31:05 -0300

    Update index.jsx

--- portfolio/src/Pages/Sobre/index.jsx
+++ portfolio/src/Pages/Sobre/index.jsx
@@ -1,17 +1,13 @@
-import Header from "../../Components/Header"
-import Footer from "../../Components/Footer"
-import Container from "../../Components/Container"
+
 
 function Sobre(){
 
     return(
         <>
        
-       <Header></Header>
-       <Container>
+       
        <h2>Sobre</h2>
-       </Container>
-        <Footer></Footer>
+     
        
         
         </>
