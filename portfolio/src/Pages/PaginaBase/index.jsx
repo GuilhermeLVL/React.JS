3bb0d60ed0198119ce30643fa866ff674e7eec14
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-04 14:36:44 -0300

    Pagina base

---
+++ portfolio/src/Pages/PaginaBase/index.jsx
@@ -0,0 +1,18 @@
+import Header from "../../Components/Header"
+import Container from "../../Components/Container"
+import Footer from "../../Components/Footer"
+
+function PageBase(){
+    return(
+        <main>
+            <Header></Header>
+            <Container>
+
+                
+            </Container>
+            <Footer></Footer>
+
+        </main>
+    )
+}
+export default PageBase
