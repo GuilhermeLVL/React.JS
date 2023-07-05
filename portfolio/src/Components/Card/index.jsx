b7e3600642f1ffbc7781e5d4c5130f60cabbc4ce
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-05 19:38:48 -0300

    Import icons

--- portfolio/src/Components/Card/index.jsx
+++ portfolio/src/Components/Card/index.jsx
@@ -1,5 +1,5 @@
 import styles from './Card.module.css'
-
+import {FaHtml5, FaReact, FaJs, FaCss3Alt} from 'react-icons/fa'
 function Card (){
 
     return(
@@ -14,7 +14,10 @@ function Card (){
 
 
                 <div className={styles.card_icones}>
-                    html
+                    <FaHtml5 ></FaHtml5>
+                    <FaCss3Alt ></FaCss3Alt>
+                    <FaJs ></FaJs>
+                    <FaReact ></FaReact>
                 </div>
 
                 <button className={styles.botao}>
