402aaae47c9e4876d035d30bb518fd029ece8128
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-06 16:17:21 -0300

    Update index.jsx

--- portfolio/src/Pages/Projetos/index.jsx
+++ portfolio/src/Pages/Projetos/index.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react"
 import Card from "../../Components/Card"
 import styles from './Projetos.module.css'
 
 function Projetos(){
+
+        const [repositories, setRespositories] = useState([])
+
+        useEffect(()=>{
+            const buscarRepositorios = async () =>{
+
+                const response = await fetch('https://api.github.com/users/GuilhermeLVL/repos')
+
+                const data = await response.json()
+
+                setRespositories(data)
+            }
+
+            buscarRepositorios()
+        }, [])
 
     return(
         <section className={styles.projeto}>
@@ -9,13 +25,18 @@ function Projetos(){
       
 
             <h2>Projetos</h2> 
-         <section className={styles.lista}> 
+      {
+
+        repositories.length > 0 ? (
+            <section className={styles.lista}> 
             <Card></Card>
             <Card></Card>
             <Card></Card>
             <Card></Card>
             <Card></Card>
          </section>
+        ) : ( <p>Carregando Repositorios...</p>)
+      }
         
         </section>
     )
