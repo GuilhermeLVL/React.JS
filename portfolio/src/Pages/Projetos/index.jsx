2948b73a95f642349b549e5a95f5e078018a29a6
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-06 15:54:18 -0300

    css projetos

--- portfolio/src/Pages/Projetos/index.jsx
+++ portfolio/src/Pages/Projetos/index.jsx
@@ -1,18 +1,23 @@
 import Card from "../../Components/Card"
+import styles from './Projetos.module.css'
 
 function Projetos(){
 
     return(
-        <>
+        <section className={styles.projeto}>
        
       
 
             <h2>Projetos</h2> 
+         <section className={styles.lista}> 
             <Card></Card>
             <Card></Card>
             <Card></Card>
+            <Card></Card>
+            <Card></Card>
+         </section>
         
-        </>
+        </section>
     )
 }
 
