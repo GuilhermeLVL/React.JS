cbd5a3e56e1a92f3840531583b0528539a7fb999
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-07-10 17:36:43 -0300

    jsx menu

--- portfolio/src/Components/Header/index.jsx
+++ portfolio/src/Components/Header/index.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 
 function Header() {
 
+const [showMenu, setShowMenu] = useState(false)
+const toggleMenu = () => {
+
+    setShowMenu(!showMenu)
+
+} 
     return(
     <header className={styles.header}>
 
         <Link to="/">
             <span>GuilhermePinheiro.Dev</span>
         </Link>
-        <nav>
+        <nav className={`${showMenu ? styles.show : '' }`}>
             <Link to="/">Home</Link>
             <Link to="/Sobre">Sobre</Link>
             <Link to="/Projetos">Projetos</Link>
@@ -18,7 +24,9 @@ function Header() {
             
         </nav>
 
-        <div className={styles.menuButton}>
+        <div className={styles.menuButton}
+        onClick={toggleMenu}
+        >
 
             <span  className={styles.linha}></span>
             <span  className={styles.linha}></span>
