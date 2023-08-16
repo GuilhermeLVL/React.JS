57b0416bd8732a9517ca4d1c6561f8685d606426
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-16 20:02:24 -0300

    .

---
+++ client/src/components/TopNav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {AiOutlineMenu,} from 'reasct-icons/ai'
+
+
+const TopNav = () => {
+  return (
+    <div className='max-w-[1520] mx-auto flex justify-between items-center p-4'>
+
+    <div className='flex items-center'>
+        <div className='cursor-pointer'>
+            <></>
+        </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default TopNav
