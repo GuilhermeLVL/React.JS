68db1f2053c20bdddcdb96b96c7e0b48068b4819
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-17 19:04:26 -0300

    Update TopNav.js

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -5,17 +5,23 @@ import {AiOutlineMenu,AiOutlineSearch,AiOutlineClose,AiFillTag} from 'react-icon
 const TopNav = () => {
   return (
     <div className='max-w-[1520] mx-auto flex justify-between items-center p-4'>
-
     <div className='flex items-center'>
+      
         <div className='cursor-pointer'>
             <AiOutlineMenu size={25}></AiOutlineMenu>
         </div>
 
-        <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>Yum
-          <span> Eats</span>
+        <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>Ponto
+          <span> Verde</span>
         </h1>
+    
+
+    <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'>
+      <p className='bg-orange-700 text-white rounded-full p-2 text-bold'>Free</p>
+      <p className='p-2 text-bold'>Display</p>
     </div>
 
+    </div>
     </div>
   )
 }
